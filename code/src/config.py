79a54caa9101cb5ddb578090f0372bd546f2766a
kong-long-bye <2968498506@qq.com>
2026-07-20 16:10:23 +0800

    1. 新增lgbm代码，不采用transformer，采用lgbm来做 2. 预测代码也换位lgbm 3. config增加了lgbm的配置 4.并未修改原来的代码

--- code/src/config.py
+++ code/src/config.py
@@ -20,4 +20,29 @@ config = {
 
     'output_dir': f'./model/{sequence_length}_{feature_num}',
     'data_path': './data',
+
+
+    'lgbm_output_dir': f'./model/lgbm_{feature_num}',
+    # LightGBM排序模型单独保存，不影响原Transformer模型
+    'lgbm_output_dir': f'./model/lgbm_{feature_num}',
+    'lgbm_early_stopping_rounds': 50,
+    'lgbm_params': {
+        # 直接优化同一交易日内的股票排序
+        'objective': 'lambdarank',
+        'n_estimators': 1000,
+        'learning_rate': 0.03,
+        'num_leaves': 31,
+        'max_depth': 6,
+        'min_child_samples': 30,
+        'subsample': 0.8,
+        'subsample_freq': 1,
+        'colsample_bytree': 0.8,
+        'reg_alpha': 1.0,
+        'reg_lambda': 5.0,
+        'random_state': 42,
+        'n_jobs': -1,
+        'importance_type': 'gain',
+        # Top5任务重点关注前8名附近的排序
+        'lambdarank_truncation_level': 8,
+    },
 }
