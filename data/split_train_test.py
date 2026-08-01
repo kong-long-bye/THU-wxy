4a6cc4ee682dad78be5e4ed641eaeaeb5ca23f92
konglong <2968498506@qq.com>
2026-08-01 22:27:04 +0800

    更新最新的数据集

--- data/split_train_test.py
+++ data/split_train_test.py
@@ -29,8 +29,8 @@ def parse_args() -> argparse.Namespace:
 	parser.add_argument(
 		"--train-end",
 		type=str,
-		default="2026-07-31",
-		help="训练集结束日期，默认 2026-07-31",
+		default="2026-07-24",
+		help="训练集结束日期，默认 2026-07-24",
 	)
 	parser.add_argument(
 		"--test-start",
